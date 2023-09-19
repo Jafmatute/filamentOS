bc388f5d03970a8649447967c0b82fd6e0cc0aa3
Jafmatute <josueari20@gmail.com>
2023-09-19 11:42:48 -0600

    Filament VouchersBelongsTo and migration, model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
@@ -15,4 +16,10 @@ class Product extends Model
         "slug",
         "image",
     ];
+
+    public function voucher() : HasMany {
+
+        return $this->hasMany(Voucher::class);
+
+    }
 }
